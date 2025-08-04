b1fcbc84f8082834989c686212c369b21ff59050
MaiaAndrey31 <maiaandrey31@outlook.com>
2025-08-04 10:57:00 -0300

    Webhook Adjust

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/webhook': {
-        target: 'https://devclubcrm.app.n8n.cloud',
+        target: 'https://n8n.rodolfomori.com.br',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/webhook/, '/webhook/181f9533-4319-4603-b713-97c42031efad'),
         secure: true,
