7d3f8dcc303a1ca10be6f5a8a2f5990cbe2a6cef
MaiaAndrey31 <maiaandrey31@outlook.com>
2025-08-05 21:50:14 -0300

    Pagination

--- src/Pages/Trophy/index.jsx
+++ src/Pages/Trophy/index.jsx
@@ -22,7 +22,7 @@ function Trophy() {
   const [pedidos, setPedidos] = useState([])
   const [filtros, setFiltros] = useState({ status: '', busca: '' })
   const [pagina, setPagina] = useState(1)
-  const [itensPorPagina] = useState(6)
+  const [itensPorPagina] = useState(5)
   const [modalPedido, setModalPedido] = useState(null)
   const [showModal, setShowModal] = useState(false)
 
