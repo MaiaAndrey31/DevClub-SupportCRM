f2e40a473a06d67c778d74dbacc15cd2325608e8
MaiaAndrey31 <maiaandrey31@outlook.com>
2025-08-12 21:20:40 -0300

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
+    historyApiFallback: true,
     proxy: {
       '/api/webhook': {
         target: 'https://n8n.rodolfomori.com.br',
